8b0554a988586e12fec8f39b312c887d711dc458
Martin Eide Lien <meidelien@gmail.com>
2022-02-14 15:49:36 +0100

    Updated and removed some stuff

--- Stuff/export formulas.py
+++
@@ -1,8 +0,0 @@
-import xml.etree.ElementTree 
-
-
-
-
-tree = xml.etree.ElementTree.parse('C:\\Users\\meide\\Documents\\GitHub\\Master\\data\\e_coli_core.xml')
-root = tree.getroot()
-print (root.find("chemicalFormula").text)
